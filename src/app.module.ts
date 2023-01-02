462f199a27cc147b0271f238a78024b520feb8e4
modesty0310 <rlatprua4@gmail.com>
2023-01-02 16:27:24 +0900

    mysql, typeorm, graphql connect

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123123123',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
