1564d891483182e8a223a489d66568bf57fd7a78
modesty0310 <rlatprua4@gmail.com>
2023-01-03 17:55:12 +0900

    join-find

--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -15,11 +15,11 @@ export class ProductService {
     ){}
 
     async findAll() {
-        return await this.productRepository.find();
+        return await this.productRepository.find({relations: ["productSaleslocation"]});
     }
 
     async findOne({productId}) {
-        return await this.productRepository.findOne({where: {id:productId}});
+        return await this.productRepository.findOne({where: {id:productId}, relations: ["productSaleslocation"]});
     }
 
     async create({createProductInput}) {
